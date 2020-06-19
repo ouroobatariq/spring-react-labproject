7ceaeb320161fa55cdf9d043393e2f8a8b4664c8
Ourooba Tariq <bcs163109@cust.pk>
2020-06-19 13:51:18 +0500

    React Final

---
+++ src/main/java/com/example/springreactlabproject/SpringReactLabprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreactlabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactLabprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactLabprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springreactlabproject/SpringReactLabprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreactlabproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactLabprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
